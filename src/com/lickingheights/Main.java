65a630e1a04a70b4b3a8bd998572d08f51e860d6
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-24 09:52:51 -0400

    broke project into steps, collected inches, converted to meters

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -1,8 +1,68 @@
 package com.lickingheights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        //todo declare the variables that we are going to use
+        double feet, inches, meters, pounds, kilograms, BMI;
+        Scanner keyboard;
+        //todo get user input to fill out the variables
+        keyboard = new Scanner(System.in);
+        System.out.println("Welcome to the BMI calculator, what is your total height in inches?");
+        inches = keyboard.nextDouble();
+        //todo convert the variable values so they will be usable in the BMI equation
+        //inch = m*(11inch/0.0254meter")
+        meters = inches * 0.0254;
+
+
+        //todo plug those variables into the BMI equation
+        //"(BMI = kg/m*m")
+        BMI = kilograms/(meters * meters);
+
+        //todo print out BMI results
+
+
+         "(m = inch *(0.0254meter")
+
+
+
+        //height= 5'6")
+        //feet= 7")
+        // height= in inches, 66")
+        ("inches= their input")
+        ("inches =inches +(feet *12")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
